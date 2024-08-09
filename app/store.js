e88f9a0ea78671ef64347bb1719c6725d1b13ad1
akhil.abraham <akhil.abraham@qburst.com>
2024-08-09 09:19:34 +0530

    async thunk with redux/toolkit

--- app/store.js
+++ app/store.js
@@ -4,8 +4,9 @@ const logger = reduxLogger.createLogger();
 
 const cakeReducer = require("../features/cake/cakeSlice");
 const iceCreamReducer = require("../features/iceCream/iceCreamSlice");
+const userReducer = require("../features/users/userSlice");
 const store = configStore({
-  reducer: { cake: cakeReducer, iceCream: iceCreamReducer },
+  reducer: { cake: cakeReducer, iceCream: iceCreamReducer,user: userReducer},
   middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
 });
 module.exports = store;
--- index.js
+++ index.js
@@ -1,20 +1,19 @@
-const store = require("./app/store")
-const cakeActions= require("./features/cake/cakeSlice").cakeActions
-const iceCreamActions= require("./features/iceCream/iceCreamSlice").iceCreamActions
-console.log("initial State",store.getState())
+const store = require('./app/store')
+const cakeActions = require('./features/cake/cakeSlice').cakeActions
+const icecreamActions =require('./features/iceCream/iceCreamSlice').iceCreamActions
+const fetchUsers = require('./features/users/userSlice').fetchUsers
 
-const unsubscribe= store.subscribe(()=>{
-    console.log(store.getState())
+console.log('Initial State ', store.getState())
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated State ', store.getState())
 })
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.restocked(3))
-store.dispatch(iceCreamActions.restocked(3))
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.ordered())
-store.dispatch(iceCreamActions.ordered())
-
-unsubscribe()
-
-
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.ordered())
+store.dispatch(icecreamActions.restocked(3))
+store.dispatch(fetchUsers())
+// unsubscribe()
